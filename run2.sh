9d7a746b090aff7e47505758bb746ae3839240ee
filippofiocchi <52209617+filippofiocchi@users.noreply.github.com>
2019-07-29 10:44:29 +0200

    Upadate run2.sh

--- run2.sh
+++ run2.sh
@@ -3,7 +3,6 @@ cd /home/pi/github-issue-thermal-printer
 PATH=/usr/local/bin:$PATH
 export TOKEN_GITHUB='your github token'
 export TOKEN_BITLY='your bitlytoken'
-export DATABASE_NAME='the name of your database'
 export NAME_ORGANIZZATION='the name of the organizzation on github'
 export FLASK_APP=flask_recovery.py
 /usr/local/bin/pipenv run flask run --host=0.0.0.0
