0961bf6e06d8248835e35646a241253d40e2c840
filippofiocchi <52209617+filippofiocchi@users.noreply.github.com>
2019-07-31 15:40:20 +0200

    Update recovery_issue.py

--- recovery_issue.py
+++ recovery_issue.py
@@ -41,7 +41,6 @@ class Printer() :
              self._printer.print((self._repository).strip()+' #'+ self._n_issue )
              self._printer.size = adafruit_thermal_printer.SIZE_MEDIUM
              self._printer.justify = adafruit_thermal_printer.JUSTIFY_LEFT
-             self._printer.feed(1)
              if len(self._title) < 16:
                  self._printer.print(self._title)
              else :
@@ -52,7 +51,6 @@ class Printer() :
              self._printer.print(self._repository[0:10]+'...')
              self._printer.size = adafruit_thermal_printer.SIZE_MEDIUM
              self._printer.justify = adafruit_thermal_printer.JUSTIFY_LEFT
-             self._printer.feed(1)
              if len(self._title) < 16:
                  self._printer.print(self._title)
              else :
